7d6415bb760a7e1c00942dc82fffdc0da0b9b469
Raraeva24 <106894564+Raraeva24@users.noreply.github.com>
2023-03-14 10:51:31 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Menghitung_Bangun_Datar
+{
+    /// <summary>
+    /// Main Menu
+    /// </summary>
+    /// <remarks>Menghitung Salah Satu Bangun Datar Berupa Persegi Panjang</remarks>
+    class persegipanjang
+    {
+        /// <summary>
+        /// input, proses, dan output data
+        /// </summary>
+        /// <param name="p">ini panjangnya</param>
+        /// <param name="l">ini luasnya</param>
+        private double p, l, luas;
+        public void inputdata()
+        {
+            Console.WriteLine("masukkan panjangnya");
+            p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("masukkan lebarnya");
+            l = Convert.ToDouble(Console.ReadLine());
+        }
+        public void prosesdata()
+        {
+            luas = p * l;
+        }
+        public void outputdata()
+        {
+            Console.WriteLine("panjang {0}, lebar {1}, luas {2}", p, l, luas);
+
+        }
+        static void Main (string[] args)
+        {
+            persegipanjang pp = new persegipanjang();
+
+            pp.inputdata();
+            pp.prosesdata();
+            pp.outputdata();
+        }
+    }
+}
